95866ab4253a9556932237706a3151e9f2bbe8db
Frozepunk <smg2750935@gmail.com>
2024-08-08 06:08:02 +0530

    correcting  the script

--- install.sh
+++ install.sh
@@ -2,7 +2,3 @@
 sudo apt update
 sudo apt install i3-wm 
 sudo apt install i3-status
-wget -O ~/Hack-Nerd-Font-Bold-Italic.ttf https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip && \
-unzip -o Hack.zip 'Hack Nerd Font*Bold*Italic*' -d ~/.local/share/fonts && \
-fc-cache -fv
-
