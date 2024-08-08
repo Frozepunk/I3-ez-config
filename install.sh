20b24e579cc5eb500254c46889aca976e2947881
Frozepunk <smg2750935@gmail.com>
2024-08-08 08:48:46 -0400

    hack nerd fonts and rofi config files added

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 sudo apt update
+sudo apt install git
+sudo apt install rofi 
 sudo apt install i3-wm 
 sudo apt install i3-status
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.0/Hack.zip
+unzip Hack.zip -d Hack
+sudo mv Hack/*.ttf /home/$USER/.local/share/fonts
+sudo fc-cache -fv
+git clone --depth=1 https://github.com/adi1090x/rofi.git
+cd rofi
+chmod +x setup.sh
+./setup.sh
